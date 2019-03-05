94fd01e9992da88a11c00175a3f911b3ec4b913f
Shyahn <shyahn@321webmarketing.com>
2019-03-05 09:27:43 -0500

    First commit of March 5th

--- website/tests.py
+++ website/tests.py
@@ -1,3 +1,5 @@
 from django.test import TestCase
+from .models import Name, Phone_Number, Message
+from .forms import ContactForm
 
 # Create your tests here.
--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .forms import NameForm, PhoneNumberForm, MessageForm, ContactForm
+from .forms import ContactForm
 
 def index(request):
     return render(request, 'website/index.html', {})
@@ -11,31 +11,7 @@ def games(request):
     return render(request, 'website/games.html', {})
 
 def contact(request):
-    #if request.method == "POST":
-        #form = NameForm(request.POST)
-       # if form.is_valid():
-         #   name_info = form.save()
-         #   name_info.save()
-    #else:
-     #   form = NameForm()
-
-    #if request.method == "POST":
-      #  form = PhoneNumberForm(request.POST)
-       # if form.is_valid():
-         #   phone_number_info = form.save()
-          #  phone_number_info.save()
-   # else:
-     #   form = PhoneNumberForm()
-
-   # if request.method == "POST":
-      #  form = MessageForm(request.POST)
-      #  if form.is_valid():
-         #   message_info = form.save()
-          #  message_info.save()
-    #else:
-       # form = MessageForm()
-
-   if request.method == "POST":
+    if request.method == "POST":
         form = ContactForm(request.POST)
         if form.is_valid():
             contact_info = form.save()
